1e4b132ca14600592d42486b88b039db5cf484b7
unknown <dickoallassane1997@gmail.com>
2024-07-14 15:17:48 +0100

    Add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/home';
+
 import Tender from './pages/Tender/Tender';
 import Payement from './pages/Payement/Payement';
 import NotFound from './pages/NotAccessible/NotFound';
 import NotAllowed from './pages/NotAccessible/NotAllowed';
+import Home from './pages/Home/home';
+import NavBar from './components/Navbar/NavBar';
 
 const App = () => {
   return (
     <div className="App">
-      
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tender" element={<Tender />} />
-          <Route path="/payement" element={<Payement />} />
-          <Route path="/NotFound" element={<NotFound />} />
-          <Route path="/NotAllowed" element={<NotAllowed />} />
-        </Routes>
-      </Router>
-
+      <NavBar></NavBar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tender" element={<Tender />} />
+        <Route path="/payement" element={<Payement />} />
+        <Route path="/NotFound" element={<NotFound />} />
+        <Route path="/NotAllowed" element={<NotAllowed />} />
+      </Routes>
     </div>
   );
 }
--- frontend/src/components/Navbar/NavBar.jsx
+++ frontend/src/components/Navbar/NavBar.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './NavBar.css'
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  
   return (
-    <div>NavBar</div>
+    <div>
+      <header>
+        <div className="logo"></div>
+        <div className="nav-menu">
+          {!isOpen? (<button className="show-menu" onClick={()=> setIsOpen(true)}>☰</button>)
+          :(<button className="close-menu" onClick={()=> setIsOpen(false)}>✖</button>)}
+          
+          
+          <ul className={isOpen? "show" :  "hidden"}>
+            <li>
+              <NavLink to="/auth">Connexion/Inscription</NavLink>
+            </li>
+            <li>
+              <NavLink to="/">Services</NavLink>
+            </li>
+            <li>
+              <NavLink to="/payement">Tarifs</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contactes</NavLink>
+            </li>
+            <li>
+              <NavLink to="/payement">S'abonner à un essaie gratuit</NavLink>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
   )
 }
 
