c45dd7b8982f2b0bb68341c357c2e590d3791212
unknown <dickoallassane1997@gmail.com>
2024-07-14 14:33:01 +0100

    Add routing to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/home';
+import Tender from './pages/Tender/Tender';
+import Payement from './pages/Payement/Payement';
+import NotFound from './pages/NotAccessible/NotFound';
+import NotAllowed from './pages/NotAccessible/NotAllowed';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tender" element={<Tender />} />
+          <Route path="/payement" element={<Payement />} />
+          <Route path="/NotFound" element={<NotFound />} />
+          <Route path="/NotAllowed" element={<NotAllowed />} />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
